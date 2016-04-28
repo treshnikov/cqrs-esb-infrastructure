cbf01e4c1260df4bd70b15803e37b2abfe02b4b2
treshnikov <treshnikov@gmail.com>
2016-04-28 10:32:39 +0400

    Обработка запроса

---
+++ WebApplication/Controllers/CqrsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+using CQRS;
+using Microsoft.Practices.Unity;
+using Newtonsoft.Json;
+using UsersService;
+
+namespace WebApplication.Controllers
+{
+    public class CqrsController : Controller
+    {
+        private readonly IUnityContainer _container;
+
+        public CqrsController(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public void Command(string service, string command, string args)
+        {
+        }
+
+        public ActionResult Query(string service, string query, string args)
+        {
+            // todo - добавить контейнер
+
+            // по имени запроса десериализовать json в объект
+            var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).ToList();
+            var queryType = types.FirstOrDefault(i => i.Name == query);
+
+            if (queryType == null)
+                throw new ArgumentException(query);
+
+            var queryObject = JsonConvert.DeserializeObject(args, queryType);
+
+            // найти обработчик для данного запроса
+            var queryHandlerType = types.FirstOrDefault(i => i.Name == query + "Handler");
+            var queryHandlerInstance = Activator.CreateInstance(queryHandlerType);
+
+            // выполнить запрос и вернуть данные
+            var method = queryHandlerType.GetMethod("Handle");
+            var result = method.Invoke(queryHandlerInstance, new object[1] { queryObject });
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -1,18 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.SessionState;
+using CQRS;
+using Microsoft.Practices.Unity;
 
 namespace WebApplication
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            IControllerFactory unityControllerFactory = new UnityControllerFactory();
+            ControllerBuilder.Current.SetControllerFactory(unityControllerFactory);
+
+        }
+
+    }
+
+    public class UnityControllerFactory : DefaultControllerFactory
+    {
+        private  readonly IUnityContainer _container;
+
+        public UnityControllerFactory()
+        {
+            _container = new UnityContainer();
+            _container.RegisterInstance<IUnityContainer>(_container);
+
+            //todo
+            //var queryHandlers = AppDomain.CurrentDomain.GetAssemblies()
+            //    .SelectMany(s => s.GetTypes())
+            //    .Where(p => !p.IsInterface && typeof(IQueryHandler<,>).IsAssignableFrom(p)).ToList();
+            //foreach (var queryHandler in queryHandlers)
+            //{
+            //    var queryType = queryHandler.GetGenericArguments()[0];
+            //    var queryResultType = queryHandler.GetGenericArguments()[1];
+            //    var t = queryHandler.GetInterface("IQueryHandler");
+            //    _container.RegisterType(t, queryHandler);
+            //}
+        }
+
+        public override IController CreateController(System.Web.Routing.RequestContext requestContext, string controllerName)
+        {
+            var controllerType = GetControllerType(requestContext, controllerName);
+            return _container.Resolve(controllerType) as IController;
         }
     }
 }

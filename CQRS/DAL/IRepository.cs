2a36c271ffe0a1f8d29691412cde7e3a446d7891
treshnikov <treshnikov@gmail.com>
2016-05-01 23:39:30 +0400

    DAL

--- CQRS/DAL/IRepository.cs
+++ CQRS/DAL/IRepository.cs
@@ -1,32 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using Newtonsoft.Json;
-
-namespace CQRS.DAL
+﻿namespace CQRS.DAL
 {
     public interface IRepository
     {
         T[] Get<T>();
-    }
-
-    public class JsonRepository : IRepository
-    {
-        public static string GetJsonFilePath(Type type)
-        {
-            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            var uri = new UriBuilder(codeBase);
-            var path = Uri.UnescapeDataString(uri.Path);
-            var res = Path.GetDirectoryName(path);
-
-            return String.Format("{0}\\..\\App_Data\\{1}", res, type.Name + ".json");
-        }
-
-        public T[] Get<T>()
-        {
-            var filePath = GetJsonFilePath(typeof (T[])).Replace("[", "").Replace("]", "");
-            return JsonConvert.DeserializeObject<T[]>(File.ReadAllText(filePath));
-        }
-
     }
 }

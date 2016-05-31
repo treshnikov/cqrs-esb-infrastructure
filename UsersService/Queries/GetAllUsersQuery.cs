2a76c50616a4fcf2ab833585a7baae9ee52559e4
treshnikov <treshnikov@gmail.com>
2016-05-31 14:17:42 +0400

    Рефакторинг запросов

--- UsersService/Queries/GetAllUsersQuery.cs
+++ UsersService/Queries/GetAllUsersQuery.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using CQRS;
+using CQRS.DAL;
 
 namespace UsersService
 {
     public class GetAllUsersQuery : IQuery<User[]>
     {
         public string ServiceName => "userService";
+    }
+
+    public class GetAllUsersQueryHandler : IQueryHandler<GetAllUsersQuery, User[]>
+    {
+        private readonly IRepository _repository;
+
+        public GetAllUsersQueryHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public User[] Handle(GetAllUsersQuery query)
+        {
+            return _repository.Get<User>();
+        }
     }
 }
--- UsersService/Queries/GetUsersWithPermissionsQuery.cs
+++ UsersService/Queries/GetUsersWithPermissionsQuery.cs
@@ -1,4 +1,7 @@
-﻿using CQRS;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CQRS;
+using CQRS.DAL;
 
 namespace UsersService
 {
@@ -6,5 +9,33 @@ namespace UsersService
     {
         public string[] Permissions { get; set; }
         public string ServiceName => "rpc";
+    }
+
+    public class GetUsersWithPermissionsQueryHandler : IQueryHandler<GetUsersWithPermissionsQuery, User[]>
+    {
+        private readonly IRepository _repository;
+
+        public GetUsersWithPermissionsQueryHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public User[] Handle(GetUsersWithPermissionsQuery query)
+        {
+            var res = new List<User>();
+            var users = new GetAllUsersQueryHandler(_repository).Handle(new GetAllUsersQuery());
+            foreach (var user in users)
+            {
+                foreach (var permission in query.Permissions)
+                {
+                    if (user.Permissions.Any(p => p.Name == permission))
+                    {
+                        if (!res.Contains(user))
+                            res.Add(user);
+                    }
+                }
+            }
+            return res.ToArray();
+        }
     }
 }

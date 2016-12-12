c2447753f93db469b4df940a40388b433d237763
treshnikov <treshnikov@gmail.com>
2016-12-12 10:02:54 +0400

    Добавлен angular, удалены View на Razor

---
+++ WebApplication/App/app.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('app', [
+    'ngRoute',
+]);
+
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'index.html',
+            controller: '',
+            resolve: {}
+        });
+});
+
+
+app.service('clientSettings', function () {
+    this.deleteCacheAfterManualRefresh = true;
+
+    this.getServerInstance = function () {
+        return "/td";
+    };
+});
--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -4,9 +4,17 @@ namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
         public ActionResult Index()
         {
-            return View();
+            // 
+            return Redirect("~/app/#/home");
+        }
+
+        public ActionResult UnauthorisedException(string message)
+        {
+            // в случае нехватки прав - редирект на главную страницу с очисткой прав
+            return Redirect("~/app/#/home");
         }
     }
 }

e0edf3c18670fecb50e85cd30419f96e944b494a
treshnikov <treshnikov@gmail.com>
2016-12-12 09:26:42 +0400

    Подготовка к имплементированию UI как часть микросервиса

---
+++ UsersService/UI/settings.js
@@ -0,0 +1,9 @@
+﻿if (modules == undefined || modules == null)
+    modules = [];
+
+modules.push(
+{
+    id: "D9ECC812-28F6-4C26-AE9B-D830B20862A6",
+    name: "Справочник пользователей",
+    permissionsNeeded: ["users"]
+});
